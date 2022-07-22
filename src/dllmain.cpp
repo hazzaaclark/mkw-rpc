3d82dd224553686aa2b757a860cd3e53fe77b776
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-07-22 20:16:00 +0100

    Add files via upload

---
+++ src/dllmain.cpp
@@ -0,0 +1,12 @@
+#include <ctime>
+#include "pch.hpp"
+#include "discord_rpc.hpp"
+#pragma warning(disable::4996)
+
+static DiscordRichPresence DiscordRPC;
+char CurrentAreaName[128]{};
+
+void Init()
+{
+
+}
