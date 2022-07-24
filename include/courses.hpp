c35382835a1751b80d39c57ab94ff5d9ddaa48ca
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-07-24 21:02:27 +0100

    Add files via upload

---
+++ include/courses.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <map>
+#include <string>
+
+std::map<std::string, const char*> COURSES =
+{
+	/** NITRO COURSES **/
+
+	{ "LUIGI CIRCUIT", "beginner_course.szs" },
+	{ "MOO MOO MEADOWS", "farm_course.szs" },
+	{ "MUSHROOM GORGE", "kinoko_course.szs" },
+	{ "TOAD'S FACTORY", "factory_course.szs" },
+	{ "MARIO CIRCUIT", "castle_course.szs" },
+	{ "COCONUT MALL", "shopping_course.szs" },
+	{ "DK'S SNOWBOARD CROSS", "boardcross_course.szs" },
+	{ "WARIO'S GOLD MINE", "truck_course.szs" },
+	{ "DAISY CIRCUIT", "senior_course.szs" },
+	{ "KOOPA CAPE", "water_course.szs" },
+	{ "MAPLE TREEWAY", "treehouse_course.szs" },
+	{ "GRUMBLE VOLCANO", "volcano_course.szs" },
+	{ "DRY DRY RUINS", "desert_course.szs" },
+	{ "MOONVIEW HIGHWAY", "ridgehighway_course.szs" },
+	{ "BOWSER CASTLE", "koopa_course.szs" },
+	{ "RAINBOW ROAD", "rainbow_course.szs" },
+
+	/** RETRO COURSES **/
+
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+	{ "", "" },
+
+};
---
+++ include/racetypes.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#include <map>
+
+enum type
+{
+	GRAND_PRIX,
+	TIME_TRIALS,
+	SOLO_VS_RACE,
+	TEAM_VS_RACE,
+	COIN_BATTLE,
+	BALLOON_BATTLE,
+};
