929b2f93c0bb7fce470affcd87595c4c98383bd5
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-07-30 18:38:18 +0100

    Update functions.hpp

--- src/functions.hpp
+++ src/functions.hpp
@@ -1,3 +1,14 @@
 #pragma once
+#include "discord_rpc.hpp"
+#include "discord_reg.hpp"
 #include <map>
 #include <string>
+
+#pragma warning(disable:4996)
+
+typedef struct BUFFERS
+{
+	const char StateBuff[128]{};
+
+
+} BUFFERS;
