cb94a14fd37deeda87a20b78bdadb833f0e27f1c
Harry <107435091+hazzaaclark@users.noreply.github.com>
2022-07-30 16:59:26 +0100

    Update dllmain.cpp

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -3,13 +3,22 @@
 #include "pch.hpp"
 #include "discord_rpc.hpp"
 #include "discord_reg.hpp"
+#include "courses.hpp"
 #pragma warning(disable::4996)
 
-static DiscordRichPresence DiscordRPC;
-char CurrentAreaName[128]{};
-
-void init()
+namespace Discord
 {
-	Discord_Initialise("993591242724212800", NULL, 1);
-	memset(&DiscordRPC, 0, sizeof(DiscordRPC));
+	static DiscordRichPresence DiscordRPC;
+	char CurrentAreaName[128]{};
+
+	void InitDiscord()
+	{
+		Discord_Initialise("993591242724212800", NULL, 1);
+		memset(&DiscordRPC, 0, sizeof(DiscordRPC));
+	}
+
+	void Init()
+	{
+
+	}
 }
